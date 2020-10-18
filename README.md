49c6b52f333be423b6c4439f4f89891c42ca0c48
Dillon H <dhobs21@gmail.com>
2020-10-17 23:12:58 -0700

    pushing for final grading, JS is broken, couldnt get it to work. Plan to update again soon

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Password_Generator
+# Password Generator
 A random password generator for Homework 3
+
+
+## More work to come...
+I had a hard time with this assignment and regret turning in my unfinished efforts. I plan to continue researching and working on this homework over the weekend and hopefully before next class I will have a working website. 
--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Variables to be used
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var passwordLength = "";
+var confirmLower;
+var confirmUpper;
+var confirmNum;
+var confirmSpecial;
+var values = "";
+
+// First prompt the user to enter character count for the password
+  var passwordLength = prompt("How many characters do you want your password to contain?");
+  
+  console.log(passwordLength);
+
+  // Confirm if they want Lowercase letters
+  var confirmLower = confirm("Do you want to use LOWERCASE letters?");
+  if (confirmLower) {
+   
+  }
+  console.log(values);
+
+  // Confirm if they want Uppercase letters
+  var confirmUpper = confirm("Do you want to use UPPERCASE letters? ");
+
+  // Confirm if they want Numbers
+  var confirmNum = confirm("Do you want to use NUMBERS?");
+
+  // Confirm if they want special characters 
+  var confirmSpecial = confirm("Do you want to use SPECIAL CHARACTERS? (eg. @ # $)");
+
+  // Loop to randomly choose password characters 
+  for(var i = 0; i <= passwordLength; i++) {
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+  }
+
+  // Display password in card
+  document.getElementById("password").value = password;
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,24 +57,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//First prompt the user to enter character count for the password
-var passwordLength = prompt("How many characters do you want your password to contain?");
-
-//Confirm if they want Lowercase letters
-var confirmLower = confirm("Do you want to use LOWERCASE letters?");
-
-var lowerCase = "qwertyuiopasdfghjklzxcvbnm";
-//Confirm if they want Uppercase letters
-var confirmUpper = confirm("Do you want to use UPPERCASE letters? ");
-
-var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
-//Confirmif they want numbers
-var confirmNum = confirm("Do you want to use NUMBERS?");
-var numbers = "1234567890";
-
-//Then confirm if they want special characters 
-var confirmSpecial = confirm("Do you want to use SPECIAL CHARACTERS? (eg. @ # $)");
-var specialCharacters = "!@#$%&*?";
-
