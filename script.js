6a7b1d72f302182a13611b9d223c3d8e03ae8482
Dillon H <dhobs21@gmail.com>
2020-10-17 19:25:32 -0700

    updated JS with prompts

--- script.js
+++ script.js
@@ -16,15 +16,19 @@ generateBtn.addEventListener("click", writePassword);
 
 //First prompt the user to enter character count for the password
 var passwordLength = prompt("How many characters do you want your password to contain?");
+
 //Confirm if they want Lowercase letters
 var confirmLower = confirm("Do you want to use LOWERCASE letters?");
+
 var lowerCase = "qwertyuiopasdfghjklzxcvbnm";
 //Confirm if they want Uppercase letters
 var confirmUpper = confirm("Do you want to use UPPERCASE letters? ");
+
 var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
 //Confirmif they want numbers
 var confirmNum = confirm("Do you want to use NUMBERS?");
 var numbers = "1234567890";
+
 //Then confirm if they want special characters 
 var confirmSpecial = confirm("Do you want to use SPECIAL CHARACTERS? (eg. @ # $)");
 var specialCharacters = "!@#$%&*?";
