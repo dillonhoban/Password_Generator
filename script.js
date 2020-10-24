d657570f9910284a83a02de109f2fad980b710bf
Dillon H <dhobs21@gmail.com>
2020-10-24 15:59:24 -0700

    pushing changes made with tutor Andrew on 10/21

--- script.js
+++ script.js
@@ -2,58 +2,80 @@
 var generateBtn = document.querySelector("#generate");
 
 //Variables to be used
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = "01223456789";
+var specialChar = "!%&,*+-./<>?~";
+var alphaLower = "qwertyuiopasdfghjklzxcvbnm";
+var alphaUpper = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
 var passwordLength = "";
 var confirmLower;
 var confirmUpper;
 var confirmNum;
 var confirmSpecial;
-var values = "";
+var wordBank = "";
+var finalPassword ="";
 
-// First prompt the user to enter character count for the password
+// function writePassword() {};
+
+// Write password to the #password input
+function generatePassword() {
+  var passwordText = document.querySelector("#password");
+
+  // First prompt the user to enter character count for the password
   var passwordLength = prompt("How many characters do you want your password to contain?");
-  
-  console.log(passwordLength);
 
+  //Adding at least one of each type after the confirm 
   // Confirm if they want Lowercase letters
   var confirmLower = confirm("Do you want to use LOWERCASE letters?");
-  if (confirmLower) {
-   
-  }
-  console.log(values);
-
+    
   // Confirm if they want Uppercase letters
-  var confirmUpper = confirm("Do you want to use UPPERCASE letters? ");
+  var confirmUpper = confirm("Do you want to use UPPERCASE letters?");
 
   // Confirm if they want Numbers
   var confirmNum = confirm("Do you want to use NUMBERS?");
 
   // Confirm if they want special characters 
   var confirmSpecial = confirm("Do you want to use SPECIAL CHARACTERS? (eg. @ # $)");
+    
+  console.log(passwordLength, confirmLower, confirmUpper, confirmNum, confirmSpecial);
+  
+  // Adding the confirmed strings to the var wordBank
+  if (confirmLower) {
+    wordBank+= alphaLower
+    var lowerCaseMin = alphaLower.charAt(Math.floor(Math.random * alphaLower.length - 1))
+    console.log(lowerCaseMin)
+  };
+  if (confirmUpper) {
+    wordBank+= alphaUpper
+    var uppercaseMin = alphaUpper.charAt(Math.floor(Math.random * alphaUpper.length - 1))
+    console.log(uppercaseMin)
+  };
+  if (confirmNum) {
+    wordBank+= number
+    var numMin = number.charAt(Math.floor(Math.random * number.length - 1))
+    console.log(numMin)
+  };
+  if (confirmSpecial) {
+    wordBank+= specialChar
+    var specialMin = specialChar.charAt(Math.floor(Math.random * specialChar.length - 1))
+    console.log(specialMin)
+  };
 
+  console.log (wordBank);
+  console.log(wordBank.split(""));
+  
   // Loop to randomly choose password characters 
-  for(var i = 0; i <= passwordLength; i++) {
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
-  }
+  
+  // for (var i = 0; i <= passwordLength; i++) {
+  //   password = password + wordBank.charAt(Math.floor(Math.random() * Math.floor(wordBank.length - 1)));
+  // }
 
   // Display password in card
   document.getElementById("password").value = password;
 
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
